8988b20f4a40e7b6e64b2fd15d1758f75d6185c5
ahmed <ahmedlearn01@gmail.com>
2023-01-03 17:09:05 +0300

    normal Crud

---
+++ src/items/Dto/create.item.dto.ts
@@ -0,0 +1,5 @@
+export class CreateDtoItem{
+    readonly name: string;
+    readonly description: string;
+    readonly qty: number
+}
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import { 
+    Controller, 
+    Get, 
+    Post, 
+    Put, 
+    Delete, 
+    Body, 
+    Req, 
+    Res, 
+    Param} from '@nestjs/common';
+import { CreateDtoItem } from './Dto/create.item.dto';
+import {Request, Response} from 'express'
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+
+    @Get()
+    findAll(){
+        return "get all items"
+    }
+
+    // using req and res opject in express but not the best way to use nest js.
+
+
+    // @Get()
+    // findAll(@Req() req: Request, @Res() res: Response): Response{
+    //     console.log(req.url);
+    //     return res.send('Hello World')
+    // }
+
+    // you can destructure the param or not
+
+    // @Get(':id')
+    // findOne(@Param() test): string{
+    //     return `item: ${test.id}`
+    // }
+
+    @Get(':id')
+    findOne(@Param('id') id): string{
+        return `item: ${id}`
+    }
+
+    @Post()
+    create(@Body() createItems: CreateDtoItem): string {
+        return `Name: ${createItems.name} Desc: ${createItems.description} Qty: ${createItems.qty}`;
+    }
+
+    @Put(':id')
+    updateItem(@Body() updateItemDto: CreateDtoItem, @Param('id') id): string{
+        return `ID: ${id}, Name: ${updateItemDto.name}`
+    }
+    @Delete(':id')
+    delete(@Param('id') id): string{
+        return `Deleted Id: ${id}`
+    }
+}
